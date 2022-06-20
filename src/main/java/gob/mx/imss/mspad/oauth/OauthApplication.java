7b9ba82f215838962d09368b8ba06f7b717f6f39
HugoHerrera1 <105305874+HugoHerrera1@users.noreply.github.com>
2022-06-20 10:54:00 -0500

    cambio para colocar un component scan

--- src/main/java/gob/mx/imss/mspad/oauth/OauthApplication.java
+++ src/main/java/gob/mx/imss/mspad/oauth/OauthApplication.java
@@ -2,7 +2,6 @@ package gob.mx.imss.mspad.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
