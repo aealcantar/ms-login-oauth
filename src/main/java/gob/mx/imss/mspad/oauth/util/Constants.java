317e19f43bde5e7c9982726a2107f7cbf9919c24
itzi <wuisi_99@hotmail.com>
2022-07-13 13:11:25 -0500

    login 10

--- src/main/java/gob/mx/imss/mspad/oauth/util/Constants.java
+++ src/main/java/gob/mx/imss/mspad/oauth/util/Constants.java
@@ -16,11 +16,11 @@ public class Constants {
 	public static final String GRANT_TYPE = "grant_type";
 	public static final String APPID = "appId";
 	public static final String CONTENTTYPE = "Content-Type";
-	public static final String USUARIO_INVALIDO = "¡Usuario inválido!";
+	public static final String USUARIO_INVALIDO = "¡Usuario inv\\u00E1lido\\u0021";
 	public static final String NUM_INTENTOS_NULO = "El valor IndNumIntentos se encuentra nulo.";
 	public static final String USUARIO_BLOQUEADO = "Usuario bloqueado por número de intentos excedidos, favor de contactar al administrador.";
-	public static final String NUMERO_INTENTOS_SUPERADO ="¡Ha superado el número de intentos! Su cuenta se ha bloquedo Intente recuperar su contraseña.";
-	public static final String CREDENCIALES_INCORRECTAS = "¡Credenciales incorrectas. Volver a intentar! Solo tiene 3 intentos";
+	public static final String NUMERO_INTENTOS_SUPERADO ="\\u00A1Ha superado el n\\u00FAmero de intentos! Su cuenta se ha bloquedo Intente recuperar su contraseña.";
+	public static final String CREDENCIALES_INCORRECTAS = "\\u00A1Credenciales incorrectas. Volver a intentar! Solo tiene 3 intentos";
 	public static final String USUARIO_INACTIVO = "Usuario inactivo, favor de contactar al administrador.";
-	public static final String CORREO_NO_REGISTRADO = "Error :  ¡Correo no registrado!";
+	public static final String CORREO_NO_REGISTRADO = "Error :  \\u00A1Correo no registrado\\u0021";
 }
